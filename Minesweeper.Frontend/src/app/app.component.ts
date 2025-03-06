88368c519d37c11b9dd7d6beae65fcc1531e44bf
Alexander.Korshunov <alexander.korshunov@advalange.com>
2025-03-06 15:07:40 +0300

    added chat

--- Minesweeper.Frontend/src/app/app.component.ts
+++ Minesweeper.Frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreationFormComponent } from './features/creation.form/creation.form.component';
 import { BoardComponent } from './features/board/board.component';
+import { ChatComponent } from "./features/chat/chat.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CreationFormComponent, BoardComponent],
+  imports: [RouterOutlet, CreationFormComponent, BoardComponent, ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
